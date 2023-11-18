6f9abddaba3a6938e2430bc310c28a7667bc3f91
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2023-11-18 20:01:38 +0900

    🔧 デプロイ設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
+/// <reference types="vite/client" />
+
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const isProd = env.NODE_ENV === 'production';
+
+  return {
+    base: isProd ? "/sympal-dev/" : "/",
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
+  };
 })
