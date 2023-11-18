72a37105940f245734934985de03b66cfa6635ec
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2023-11-18 18:28:24 +0900

    🎉 セットアップ

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import GlobalHeader from '@/components/global/GlobalHeader.vue';
+import GlobalFooter from '@/components/global/GlobalFooter.vue';
+</script>
+
+<template>
+  <div>
+    <header>
+      <GlobalHeader />
+    </header>
+    <main>
+      <RouterView />
+    </main>
+    <footer>
+      <GlobalFooter />
+    </footer>
+  </div>
+</template>
