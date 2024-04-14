fb0d3eacdb08d3504cab07ca263bea78aba811b6
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2024-04-14 23:13:07 +0900

    Re:push キャッシュ削除

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import DefaultLayout from '@/layouts/DefaultLayout.vue';
-</script>
-
-<template>
-  <div>
-    <DefaultLayout />
-  </div>
-</template>
--- src/components/partials/global/GlobalFooter.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="bg-red p-10 text-center text-white">&copy; Sympal Inc.</div>
-</template>
