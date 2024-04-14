1acf76672c45940b562e19f2265805c6c4063737
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2024-04-14 23:06:23 +0900

    🔧 huskeyテスト

--- src/components/pages/Home/HomeFirstView.vue
+++ src/components/pages/Home/HomeFirstView.vue
@@ -1,42 +1,44 @@
 <script setup lang="ts">
-import imgTopicIntern from "@/assets/images/topic-intern.png";
-import 'vue3-carousel/dist/carousel.css';
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import imgTopicIntern from '@/assets/images/topic-intern.png'
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination } from 'vue3-carousel'
 
 const TOPICS = [
   {
-    title: "インターン募集中",
+    title: 'インターン募集中',
     img: imgTopicIntern,
-    to: '/about',
+    to: '/about'
   },
   {
-    title: "インターン募集中",
+    title: 'インターン募集中',
     img: imgTopicIntern,
-    to: '/service',
+    to: '/service'
   },
   {
-    title: "インターン募集中",
+    title: 'インターン募集中',
     img: imgTopicIntern,
-    to: '/news',
+    to: '/news'
   },
   {
-    title: "インターン募集中",
+    title: 'インターン募集中',
     img: imgTopicIntern,
-    to: 'https://sympal.co.jp/contact/',
-  },
-];
+    to: 'https://sympal.co.jp/contact/'
+  }
+]
 </script>
 
 <template>
-  <div class="w-full h-dvh bg-cover bg-top">
+  <div class="h-dvh w-full bg-top bg-cover p-0">
     <div class="section-wrapper relative h-full">
       <!-- トップメッセージ -->
-      <div class="text-white rounded-2xl absolute top-[40%]">
-        <p class="text-4xl font-bold">ペットの健康寿命を<span class="text-red">10年</span>延ばします</p>
+      <div class="absolute top-[40%] rounded-2xl text-white">
+        <p class="text-4xl font-bold">
+          ペットの健康寿命を<span class="text-red">10年</span>延ばします
+        </p>
         <p>Sympal株式会社は、ペットフードの開発を通じてペットの健康的な生活を実現します。</p>
       </div>
       <!-- トピックカルーセル -->
-      <div class="absolute top-[60%] right-0 w-[800px]">
+      <div class="absolute right-0 top-[60%] w-[800px]">
         <Carousel :autoplay="5000" :wrap-around="true">
           <Slide v-for="slide in TOPICS" :key="slide">
             <div class="carousel__item">
@@ -54,5 +56,4 @@ const TOPICS = [
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
